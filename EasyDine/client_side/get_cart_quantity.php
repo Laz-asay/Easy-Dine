3a44c87e48268414020537186ab16931882fac3f
Lazarus Asay <lazarus.asay@gmail.com>
2025-01-19 23:04:40 +0800

    included phpinfo.php

--- EasyDine/client_side/get_cart_quantity.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-include "session.php";
-
-$totalQuantity = isset($_SESSION['total_quantity']) ? $_SESSION['total_quantity'] : 0;
-echo json_encode(['total_quantity' => $totalQuantity]);
--- EasyDine/client_side/update_cart_quantity.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "session.php";
-
-$data = json_decode(file_get_contents('php://input'), true);
-if (isset($data['total_quantity'])) {
-    $_SESSION['total_quantity'] = $data['total_quantity'];
-}
